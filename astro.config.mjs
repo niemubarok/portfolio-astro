27c1f0a8797414f769ba4a34c7900328eea4e46b
niemubarok <niemubarok@gmail.com>
2026-09-02 20:19:51 +0700

    Update project

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [
+    tailwind({
+      configFile: './tailwind.config.mjs',
+      applyBaseStyles: false,
+    }),
+    react(),
+  ]
 });
